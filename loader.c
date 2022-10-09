95f3fcef87a06ace7ac86ebde20fa55492a1c1be
Anthony Viriya <anthonyviriya98@gmail.com>
2022-10-09 18:22:07 +0100

    Update loader.c

--- loader.c
+++ loader.c
@@ -10,7 +10,7 @@
 
 int main(int argc, char **argv) {
     if (argc < 2) {
-        fprintf(stdout, "Usage: %s /path/to/binary [arg1] [arg2]\n", argv[0]);
+        fprintf(stdout, "Usage: %s \"/path/to/binary [arg1] [arg2]\"\n", argv[0]);
         exit(1);
     }
     char* new_argv[] = {"/bin/sh", "-c", argv[1], NULL};
